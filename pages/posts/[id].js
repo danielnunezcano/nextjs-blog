985aba848097a610ca0529f1716e6d255089f8d3
Daniel Nuñez Cano <daniel.nunezcano@plexus.local>
2021-08-16 12:09:23 +0200

    update

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,25 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
 
 export async function getStaticProps({ params }) {
   const postData = getPostData(params.id);
@@ -16,13 +36,4 @@ export async function getStaticPaths() {
     paths,
     fallback: false,
   };
-}
-
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {" "}
-      {postData.title} <br /> {postData.id} <br /> {postData.date}{" "}
-    </Layout>
-  );
 }
